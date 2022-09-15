b04027321acfa5b812a967ad4493b3d93ad2ea1e
Kjartan Thor Wikfeldt <ktwikfeldt@gmail.com>
2022-09-15 13:44:24 +0200

    update conf

--- content/conf.py
+++ content/conf.py
@@ -17,8 +17,8 @@
 
 # -- Project information -----------------------------------------------------
 
-project = "LESSON NAME"
-copyright = "2021, The contributors"
+project = "VASP in HPC"
+copyright = "2022, Weine Olovsson and contributors"
 author = "The contributors"
 github_user = "ENCCS"
 github_repo_name = ""  # auto-detected from dirname if blank
